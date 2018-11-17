7923632a2d9f41ec012325d568525580170d88e1
Erdong Feng <edfeng@thoughtworks.com>
2018-11-17 16:04:28 +0800

    played with named/default arguments; string template; data class; nullable; smart cast; extension function;

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -2,10 +2,9 @@ package i_introduction._10_Object_Expressions
 
 import util.TODO
 import util.doc10
-import java.util.*
 
 fun todoTask10(): Nothing = TODO(
-    """
+        """
         Task 10.
         Read about object expressions that play the same role in Kotlin as anonymous classes in Java.
 
@@ -13,11 +12,9 @@ fun todoTask10(): Nothing = TODO(
         In Kotlin you use Kotlin library extensions instead of java.util.Collections,
         but this example is still a good demonstration of mixing Kotlin and Java code.
     """,
-    documentation = doc10()
+        documentation = doc10()
 )
 
 fun task10(): List<Int> {
-    val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
-    return arrayList
+    return listOf(1, 2, 5).sortedDescending()
 }
