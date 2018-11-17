3ee4eb6c1747b77be905d5092bf172f258cba4c8
Erdong Feng <edfeng@thoughtworks.com>
2018-11-17 23:06:48 +0800

    played with operator override

--- src/iii_conventions/n29OperatorsOverloading.kt
+++ src/iii_conventions/n29OperatorsOverloading.kt
@@ -1,10 +1,11 @@
 package iii_conventions
 
+import iii_conventions.TimeInterval.WEEK
+import iii_conventions.TimeInterval.YEAR
 import util.TODO
-import iii_conventions.TimeInterval.*
 
 fun todoTask29(): Nothing = TODO(
-    """
+        """
         Task 29.
         Implement a kind of date arithmetic. Support adding years, weeks and days to a date.
         Use classes 'MyDate' and 'TimeInterval'.
@@ -15,17 +16,29 @@ fun todoTask29(): Nothing = TODO(
         (2). Support adding several time intervals to a date. Add an extra class.
         If you have any problems, see the iii_conventions/_29_Tips.kt file.
     """,
-    references = { date: MyDate, timeInterval: TimeInterval ->
-        date.addTimeIntervals(timeInterval, 1)
-    })
+        references = { date: MyDate, timeInterval: TimeInterval ->
+            date.addTimeIntervals(timeInterval, 1)
+        })
 
 fun task29_1(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR + WEEK
+    return today + YEAR + WEEK
 }
 
 fun task29_2(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + TimeInterval.DAY * 5
 }
+
+private operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
+
+private operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
+    return this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.i)
+}
+
+private operator fun TimeInterval.times(i: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(this, i)
+}
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val i: Int)
 
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,26 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var invoked: Int = 0
+    fun getNumberOfInvocations(): Int {
+        return this.invoked
+    }
+
+    operator fun invoke(): Invokable {
+        this.invoked++
+        return this
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
